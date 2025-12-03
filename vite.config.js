649562048c90b1111c3a7bd8f49d28a05e99d929
Umer Bin Ijaz <uk302269@gmail.com>
2025-12-04 03:47:05 +0500

    Set Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fess-frontend/'   // <-- important for GitHub Pages
+  assetsInclude: ['**/*.PNG'],
+  base: '/fess-frontend/'
 })
